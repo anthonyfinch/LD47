2371eeb8dafd15def7bbdee6a59048be7b6acb5e
Anthony Finch <t@anthonyfinch.com>
2020-10-03 10:39:42 +0100

    sets up overall game state, factors for multiple enemies

--- scenes/game/Game.gd
+++ scenes/game/Game.gd
@@ -2,13 +2,20 @@ extends Spatial
 
 
 export (NodePath) var people_path := "People"
+export (NodePath) var enemies_path := "Enemies"
 export (NodePath) var grid_path := "GridMap"
 onready var _people := get_node(people_path)
+onready var _enemies := get_node(enemies_path)
 onready var _grid := get_node(grid_path)
 var _selected_person: Spatial = null
+var _state = GameStates.AWAITING_PLAYER
 
-onready var _enemy = get_node("Enemies/Enemy")
-var _player_go = true
+
+
+enum GameStates {
+	AWAITING_PLAYER,
+	ENEMY_TURN
+}
 
 
 func _ready():
@@ -22,6 +29,7 @@ func _find_person(pos):
 		if grid_pos == pos:
 			return person
 
+
 func _do_move(pos):
 	if _selected_person == null:
 		var clicked = _find_person(pos)
@@ -32,7 +40,7 @@ func _do_move(pos):
 		# print("moving")
 		_selected_person.move_to(pos)
 		_selected_person = null
-		_player_go = false
+		_state = GameStates.ENEMY_TURN
 
 
 func _drop_tile(pos):
@@ -42,28 +50,31 @@ func _drop_tile(pos):
 
 
 func _do_enemy_move():
-	var enemy_pos = Vector2(_enemy.transform.origin.x, _enemy.transform.origin.z)
-	var closest_person = null
-	var closest_distance = 0
-	var vec = null
-	for person in _people.get_children():
-		var p_person = Vector2(person.transform.origin.x, person.transform.origin.z)
-		vec = p_person - enemy_pos
-		var distance = vec.length_squared()
-		if closest_person == null or distance < closest_distance:
-			closest_person = person
-			closest_distance = distance
 
-	# print("closest person ", closest_person)
+	for enemy in _enemies.get_children():
+		var enemy_pos = Vector2(enemy.transform.origin.x, enemy.transform.origin.z)
+		var closest_person = null
+		var closest_distance = 0
+		var vec = null
+		for person in _people.get_children():
+			var p_person = Vector2(person.transform.origin.x, person.transform.origin.z)
+			vec = p_person - enemy_pos
+			var distance = vec.length_squared()
+			if closest_person == null or distance < closest_distance:
+				closest_person = person
+				closest_distance = distance
 
-	# print(vec)
-	var offset = Vector2(floor(clamp(vec.x, -1, 1)), floor(clamp(vec.y, -1, 1)))
-	_enemy.move_by(offset)
-	_player_go = true
+		# print("closest person ", closest_person)
+
+		# print(vec)
+		var offset = Vector2(floor(clamp(vec.x, -1, 1)), floor(clamp(vec.y, -1, 1)))
+		enemy.move_by(offset)
+
+	_state = GameStates.AWAITING_PLAYER
 
 
 func _on_grid_clicked(pos):
-	if _player_go:
+	if _state == GameStates.AWAITING_PLAYER:
 		_do_move(pos)
 	else:
 		_drop_tile(pos)
@@ -71,5 +82,4 @@ func _on_grid_clicked(pos):
 
 
 func _on_grid_position(pos):
-	# print("over")
 	pass
