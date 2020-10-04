42ed361752178cfbc45121bd035c2c700c007109
Anthony Finch <t@anthonyfinch.com>
2020-10-04 23:04:22 +0100

    last minute fix of loading loop

--- scenes/End.gd
+++ scenes/End.gd
@@ -4,8 +4,13 @@ extends Node2D
 export (NodePath) var button_path := "Button"
 onready var _button := get_node(button_path)
 
-export (PackedScene) var level = null
+export (String, FILE) var level = null
 
 
 func _ready():
-	_button.connect("pressed", SceneSwitcher, "goto_scene", [level])
+	_button.connect("pressed", self, "_end")
+
+
+func _end():
+	var s = ResourceLoader.load(level)
+	SceneSwitcher.goto_scene(s)
